e148d470116e28d44b8301498f2450e217421728
alumnofabrizio <127007382+alumnofabrizio@users.noreply.github.com>
2024-06-12 11:19:22 -0400

    Add files via upload

--- CALCULADORA.py
+++ CALCULADORA.py
@@ -6,4 +6,9 @@ while(True):
     print("2.- Restar")
     print("")
     op=int(input("Ingrese una opcion : "))
+    if op==1:
+        n1=int(input("Ingrese numero 1 : "))
+        n2=int(input("Ingrese numero 2 : "))
+        total = n1+n2
+        print("el resultado es  : ",total)
     
