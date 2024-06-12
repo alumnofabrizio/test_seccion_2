c0395d300eccdd9382f4d6584fbb560017f6bc14
alumnofabrizio <127007382+alumnofabrizio@users.noreply.github.com>
2024-06-12 11:12:48 -0400

    Calculadora principal

---
+++ CALCULADORA.py
@@ -0,0 +1,9 @@
+while(True):
+    print("")
+    print(".-.- m e n u .-.-.-")
+    print("")
+    print("1.- Sumar")
+    print("2.- Restar")
+    print("")
+    op=int(input("Ingrese una opcion : "))
+    
